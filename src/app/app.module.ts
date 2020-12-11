ea55e578bd94b42f61828221b9a91691e657e75e
swainsa1 <santosh.swain@gmail.com>
2020-12-11 02:02:15 -0600

    first draft

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { FormsModule }   from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/contact/contact.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent implements OnInit {
+  name:string;
+  address:string;
+  phone:string;
+  email:string;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+    /**
+   * Process the form we have. Send to whatever backend
+   * Only alerting for now
+   */
+  processForm() {
+    const allInfo = `My name is ${this.name}. My email is ${this.email}. My Address is ${this.address} My phone is ${this.phone}` ;
+    console.log(allInfo);
+    
+  }
+
+}
